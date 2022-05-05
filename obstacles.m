48e8c872f5dd5a7217613ed8c487faf638615fe6
MatthewShaw2000 <34135569+MatthewShaw2000@users.noreply.github.com>
2022-05-05 15:05:18 +0100

    Assessment code KF4042

--- obstacles.m
+++ obstacles.m
@@ -1,11 +1,21 @@
 %%Obstacles
 %Add cuboid obstacles, representing buildings, to the scenario
 % Locations of the obstacles
-ObstaclePositions = [10 5; 20 0; 35 5; 25 10; 25 20; 20 25; 15 30; 10 25]; 
+
+%test 1
+%ObstaclePositions = [10 5; 20 0; 35 5; 25 10; 25 20; 20 25; 15 30; 10 25]; 
+
+%test 2
 %ObstaclePositions = [5 5; 25 5; 10 10; 25 15; 35 15; 30 25; 20 25; 10 30];
+
+%test 3
 %ObstaclePositions = [5 25; 10 20; 10 30; 15 25; 20 30; 25 15; 25 25; 30 20];
+
+%test 4
 %ObstaclePositions = [5 15; 10 10; 10 20; 15 5; 15 25; 20 10; 20 20; 25 15];
-%ObstaclePositions = [5 5; 10 0; 15 10; 20 0; 25 5; 25 20; 30 10; 35 5];
+
+%test 5
+ObstaclePositions = [5 5; 10 0; 15 10; 20 0; 25 5; 25 20; 30 10; 35 5];
 
 
 for i = 1:size(ObstaclePositions,1)
